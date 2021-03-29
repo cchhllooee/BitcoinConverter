40f392851eaaa7c40dca5075ae40d3a55c91c1e2
Chloe Hawthorne <cmhawthorne02@gmail.com>
2021-03-29 13:29:27 +0100

    changes to allow for code coverage

---
+++ BitcoinConverter.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using BitcoinConverter.Code;
+
+namespace BitcoinConverter.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var converter = new ConverterSvc();
+
+            while(true)
+            {
+                try
+                {
+                    Console.Write("Coins: ");
+                    var coins = Double.Parse(Console.ReadLine());
+                    Console.Write("Currency (USD, GBP, or EUR");
+                    var currency = (ConverterSvc.Currency)Enum.Parse(typeof(ConverterSvc.Currency), Console.ReadLine(), true);
+
+                    var amount = await converter.BitcoinConverter(currency, coins);
+
+                    Console.WriteLine($"{coins} Bitcoins = {amount} {currency}");
+
+                }
+                catch
+                {
+                   // swallow
+                }
+            }
+        }
+    }
+}
